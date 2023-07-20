8a620cbdce099453e3bb2b4768d81001f6d62e99
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-07-20 22:54:42 +0600

    Step 6: add "brain_calc.py" game and script brain-calc

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+
+import brain_games.games_logic as game
+from random import randrange, choice
+
+
+def main():
+    game_rule = 'What is the result of the expression?'
+
+    viable_operands = ['+', '-', '*']
+    max_num = 21
+    questions_num = 3
+    left_nums = []
+    operands = []
+    right_nums = []
+    for _ in range(questions_num):
+        left_nums.append(randrange(max_num))
+        operands.append(choice(viable_operands))
+        right_nums.append(randrange(max_num))
+
+    questions = []
+    right_answers = []
+    for i in range(questions_num):
+        questions.append(f'{left_nums[i]} {operands[i]} {right_nums[i]}')
+        match operands[i]:
+            case '+':
+                right_answers.append(str(int(left_nums[i]) + int(right_nums[i])))
+            case '-':
+                right_answers.append(str(int(left_nums[i]) - int(right_nums[i])))
+            case'*':
+                right_answers.append(str(int(left_nums[i]) * int(right_nums[i])))
+
+
+    game.play_game(game_rule, questions, right_answers)
+
+
+if __name__ == '__main__':
+    main()
