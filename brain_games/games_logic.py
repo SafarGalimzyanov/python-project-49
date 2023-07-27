d540cf0c14dc2609ba6b74ab8bdd245c860fbf69
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-07-27 23:59:59 +0600

    Fix line mistake games_logic.py

--- brain_games/games_logic.py
+++ brain_games/games_logic.py
@@ -17,8 +17,7 @@ def play_game(game_rule: str, game_questions: list, right_answers: list):
         if user_answer == right_answers[i]:  # continue
             print('Correct!')
         else:  # exit
-            print(f'{user_answer} is a wrong answer ;(.\
-                 Correct answer was "{right_answers[i]}".\
+            print(f'{user_answer} is a wrong answer ;(. Correct answer was "{right_answers[i]}".\
                  \nLet`s try again, {user_name}!')
             return
 
