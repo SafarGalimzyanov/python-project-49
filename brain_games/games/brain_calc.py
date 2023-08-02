46338d63a64b4d2fdafc6e71931a3ad7693a5fe7
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-08-03 03:01:01 +0600

    Improving project. Step 1: simplification of data interaction (user_interaction is input_output logic, games/ is a package including functionality of each game, scripts/ is a package including configuration data)

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python3
 
-
-import brain_games.games_logic as game
+import brain_games.games.user_interaction as u_i
 from random import randrange, choice
 
 
-def main():
-    game_rule = 'What is the result of the expression?'
-
-    viable_operands = ['+', '-', '*']
-    max_num = 21
-    left_nums = []
-    operands = []
-    right_nums = []
-    for _ in range(game.GAME_DURATION):
-        left_nums.append(int(randrange(max_num)))
-        operands.append(choice(viable_operands))
-        right_nums.append(randrange(max_num))
-
+def get_questions_answers(left_operands: list, operators: list, right_operands: list) -> tuple:
     questions = []
     right_answers = []
-    for i in range(game.GAME_DURATION):
-        questions.append(f'{left_nums[i]} {operands[i]} {right_nums[i]}')
-        match operands[i]:
+    for i in range(u_i.GAME_DURATION):
+        questions.append(f'{left_operands[i]} {operators[i]} {right_operands[i]}')
+        match operators[i]:
             case '+':
-                right_answers.append(str(left_nums[i] + right_nums[i]))
+                right_answers.append(str(left_operands[i] + right_operands[i]))
             case '-':
-                right_answers.append(str(left_nums[i] - right_nums[i]))
+                right_answers.append(str(left_operands[i] - right_operands[i]))
             case'*':
-                right_answers.append(str(left_nums[i] * right_nums[i]))
+                right_answers.append(str(left_operands[i] * right_operands[i]))
 
-    game.play_game(game_rule, questions, right_answers)
+    return (tuple(questions), tuple(right_answers))
 
 
-if __name__ == '__main__':
-    main()
+def get_random_operands(max_operand: int) -> tuple:
+    left_operands = []
+    right_operands = []
+    for _ in range(u_i.GAME_DURATION):
+        left_operands.append(int(randrange(max_operand)))
+        right_operands.append(int(randrange(max_operand)))
+
+    return (tuple(left_operands), tuple(right_operands))
+
+
+def get_random_operators(viable_operators: tuple) -> tuple:
+    operators = []
+    for _ in range(u_i.GAME_DURATION):
+        operators.append(choice(viable_operators))
+
+    return tuple(operators)
+
+
+def init_game(game_rule: str, viable_operators: tuple, max_operand: int) -> None:
+    left_operands, right_operands = get_random_operands(max_operand)
+    operators = get_random_operators(viable_operators)
+    questions, right_answers = get_questions_answers(left_operands, viable_operators, right_operands)
+    u_i.play_game(game_rule, questions, right_answers)
--- brain_games/games/user_interaction.py
+++ brain_games/games/user_interaction.py
@@ -4,21 +4,21 @@ from prompt import string
 GAME_DURATION = 3  # the game ends after 3 correct answers in a row
 
 
-def play_game(game_rule: str, game_questions: list, right_answers: list):
+def play_game(game_rule: str, questions: list, right_answers: list):
     print('Welcome to the Brain Games!')
     user_name = string('May I have your name? ')
     print(f'Hello, {user_name}!')
     print(f'{game_rule}')
 
     for i in range(GAME_DURATION):
-        print(f'Question: {game_questions[i]}')
+        print(f'Question: {questions[i]}')
         user_answer = string('Your answer: ')
 
         if user_answer == right_answers[i]:  # continue
             print('Correct!')
         else:  # exit
             print(f'"{user_answer}" is a wrong answer ;(. Correct answer was "{right_answers[i]}".')
-            print(f"Let's try again, Tirion!")
+            print(f"Let's try again, {user_name}!")
             return
 
     print(f'Congratulations, {user_name}!')
