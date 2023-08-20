380738f6fa2d835578d4ff373544e0ad0ff94c76
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-08-20 14:52:41 +0600

    Improving project. Step 9: brain_calc.py (answer is calculated in the function get_equation_result)

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -5,6 +5,17 @@ MAX_NUM = 20
 VIABLE_OPERATORS = ('+', '-', '*')
 
 
+def get_equation_result(operator: str, left_operand: int, right_operand: int) -> int:
+    match operator:
+        case '+':
+            result = str(left_operand + right_operand)
+        case '-':
+            result = str(left_operand - right_operand)
+        case'*':
+            result = str(left_operand * right_operand)
+    return result
+
+
 def get_question_answer(get_random_num, get_random_elem) -> tuple:
     def inner():
         left_operand = get_random_num(MIN_NUM, MAX_NUM)
@@ -12,14 +23,7 @@ def get_question_answer(get_random_num, get_random_elem) -> tuple:
         operator = get_random_elem(VIABLE_OPERATORS)
 
         question = (f'{left_operand} {operator} {right_operand}')
-
-        match operator:
-            case '+':
-                answer = str(left_operand + right_operand)
-            case '-':
-                answer = str(left_operand - right_operand)
-            case'*':
-                answer = str(left_operand * right_operand)
+        answer = get_equation_result(operator, left_operand, right_operand)
 
         return (question, answer)
     return inner
