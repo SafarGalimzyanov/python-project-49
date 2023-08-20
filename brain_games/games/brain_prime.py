ed5788c6fc23127777b3680c2460c70af8515325
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-08-20 14:44:14 +0600

    Improving project. Step 8: brain_prime.py (replace tuple with prime numbers with function is_prime)

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -1,15 +1,24 @@
+from math import sqrt
+
+
 # description
 GAME_RULE = 'Answer "yes" if the given number is prime, otherwise answer "no"'
-PRIME_NUMBERS = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97)
-MIN_NUM = PRIME_NUMBERS[0]
-MAX_NUM = PRIME_NUMBERS[-1]
+MIN_NUM = 1
+MAX_NUM = 99
+
+
+def is_prime(n: int) -> bool:
+    for i in range(2, int(sqrt(n) + 1)):
+        if n % i == 0:
+            return False
+    return True
 
 
 def get_question_answer(get_random_num) -> tuple:
     def inner():
         random_num = get_random_num(MIN_NUM, MAX_NUM)
         question = random_num
-        answer = "yes" if random_num in PRIME_NUMBERS else "no"
+        answer = "yes" if is_prime(question) else "no"
 
         return (question, answer)
     return inner
