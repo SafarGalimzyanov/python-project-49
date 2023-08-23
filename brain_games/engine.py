43c4e34f93a906dd514d3b4a225be6209fcc4bae
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-08-24 00:13:41 +0600

    Improving project. Step 15: engine.py (remove else ; rename get_question_answer to CamelCase version)

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,22 +4,21 @@ from prompt import string
 GAME_DURATION = 3  # the game ends after 3 correct answers in a row
 
 
-def play_game(GAME_RULE: str, get_question_answer) -> None:
+def play_game(GAME_RULE: str, GetQuestionAnswer) -> None:
     print('Welcome to the Brain Games!')
     user_name = string('May I have your name? ')
     print(f'Hello, {user_name}!\n{GAME_RULE}')
 
     for _ in range(GAME_DURATION):
-        question, right_answer = get_question_answer()
+        question, right_answer = GetQuestionAnswer()
         print(f'Question: {question}')
         user_answer = string('Your answer: ')
 
-        if user_answer == right_answer:  # continue
-            print('Correct!')
-        else:  # exit
+        if user_answer != right_answer:  # exit
             print(f'"{user_answer}" is a wrong answer ;(', end='')
             print(f'Correct answer was "{right_answer}".')
             print(f"Let's try again, {user_name}!")
             return
 
+        print('Correct!')
     print(f'Congratulations, {user_name}!')
