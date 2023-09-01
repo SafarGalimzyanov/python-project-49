3196ea85f6b88c092394da524fc6a3451c02c8b3
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-09-01 21:03:56 +0600

    Improving project. Step 17.6: utils.py (replace l with arr)

--- brain_games/games/utils.py
+++ brain_games/games/utils.py
@@ -5,7 +5,8 @@ def get_random_num(min_num: int, max_num: int) -> int:
     return randrange(min_num, max_num + 1)
 
 
-def get_random_elem(l: list, index=False):
+def get_random_elem(arr: list, index=False):
     if index:
-        return (l[randrange(len(l))], randrange(len(l)))
-    return choice(l)
+        i = randrange(len(arr))
+        return (arr[i], i)
+    return choice(arr)
