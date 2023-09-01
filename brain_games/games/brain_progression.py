d9813d070357366429e3dad80171b6b328625dcf
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-09-01 21:11:59 +0600

    Improving project. Step 17.7: brain_progression.py (add whitespace around operators)

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -11,12 +11,12 @@ PROGRESSION_LENGTH = 10
 
 
 def get_progression(start: int, step: int, length: int) -> List[str]:
-    return [str(start + step*i) for i in range(length)]
+    return [str(start + step * i) for i in range(length)]
 
 
 def get_question_answer() -> Tuple[str, str]:
-    start = get_random_num(MIN_START, MIN_START*DEVIATION)
-    step = get_random_num(MIN_STEP, MIN_STEP*DEVIATION)
+    start = get_random_num(MIN_START, MIN_START * DEVIATION)
+    step = get_random_num(MIN_STEP, MIN_STEP * DEVIATION)
 
     question = get_progression(start, step, PROGRESSION_LENGTH)
     hide_elem, hide_elem_index = get_random_elem(question, index=True)
